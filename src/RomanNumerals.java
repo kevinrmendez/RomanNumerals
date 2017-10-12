24e0f694a2d0f12521ab02130b61c04876879cef
Kevin Mendez <kevinrmendez@gmail.com>
2017-10-12 14:53:51 +0300

    class exercise RomanNumerals incomplete

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,62 @@
 
 public class RomanNumerals {
+	int counter;
+	String[] romanNumArray;
+	
+	
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+		int total= 0;
+		int number;
+		String romanNumUpperCase = romanNum.toUpperCase();
+		romanNumArray = romanNumUpperCase.split("");
+		 
+		
+		for(int i = 0; i < romanNumArray.length; i++) {
+			number = valueConverter(romanNumArray[i]);
+			total += number;
+			
+		}
+//		System.out.println(total);
+		return total;
 		
 	}
+//	public int romanNumberCounter(String romanNum) {
+//		String temp;
+//		do {
+//			counter++;
+//			temp = romanNum;
+//			
+//		}while(temp == romanNum);
+//		return counter;
+//	}
+	public int valueConverter(String romanNum) {
+		
+		switch(romanNum) {
+		case "I":
+			return 1;
+		case "V":
+			return 5;
+		case "X":
+			return 10;
+		case "L":
+			return 50;
+		case "C":
+			return 100;
+		case "D":
+			return 500;
+		case "M":
+			return 1000;
+		default:
+			return 0;
+		}
+		
+	}
+	public boolean previousNumberComperator(String previousNumber, String currentNumber) {
+		if(previousNumber == currentNumber) {
+			return true;
+		}
+		return false;
+	}
+	
+	
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,8 +5,48 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConvertRomanNumberOne() {
+		int number;
+		RomanNumerals romanNumber = new RomanNumerals();
+		number = romanNumber.convertToInteger("I");
+		assertEquals("I roman number must return 1", 1, number);
+		
+	}
+
+	@Test
+	public void testValueConverter() {
+		int number1;
+		int number5;
+		int number10;
+		RomanNumerals romanNumber = new RomanNumerals();
+		number1 = romanNumber.valueConverter("I");
+		number5 = romanNumber.valueConverter("V");
+		number10 = romanNumber.valueConverter("X");
+		assertEquals("III roman number must return 1",1,number1);
+		assertEquals("III roman number must return 5",5,number5);
+		assertEquals("III roman number must return 10",10,number10);
+		
+	}
+	@Test
+	public void testpreviousNumberCounter() {
+		String number1 = "I";
+		String number2 = "I";
+		
+		boolean comparison;
+		RomanNumerals romanNumber = new RomanNumerals();
+		comparison = romanNumber.previousNumberComperator(number1, number2);
+		assertTrue("Previous number is same as curren number",comparison );
+	}
+	@Test
+	public void testConvertToInteger() {
+		int number;
+		int number2;
+		RomanNumerals romanNumber = new RomanNumerals();
+		number = romanNumber.convertToInteger("III");
+		number2 = romanNumber.convertToInteger("VI");
+		System.out.println(number);
+		assertEquals("III roman number must return 3",3,number);
+		assertEquals("VI roman number must return 6",6,number2);
 	}
 
 }
